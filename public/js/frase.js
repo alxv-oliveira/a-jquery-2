43e3e493bffdf089c3c7fc09a6779369c74eec64
Alexander Victor <alxv.oliveira@gmail.com>
2020-02-18 14:08:09 -0300

    06 && 07.Same Origin Policy e CORS && Plugins do jQuery

--- public/js/frase.js
+++ public/js/frase.js
@@ -2,16 +2,16 @@ $("#botao-frase").click(fraseAleatoria);
 $("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria() {
-    $("#spinner").toggle()
-
+	$("#spinner").toggle();
     $.get("http://localhost:3000/frases", trocaFraseAleatoria)
     .fail(function(){
-        $("#erro").toggle();
-        setTimeout(function(){
-            $("#erro").toggle();
-        },2000);
-    }).always(function () {
-        $("#spinner").toggle()
+    	$("#erro").toggle();
+    	setTimeout(function(){
+    		$("#erro").toggle();
+    	},1500);
+    })
+    .always(function(){
+    	$("#spinner").toggle();
     });
 }
 
@@ -24,27 +24,34 @@ function trocaFraseAleatoria(data) {
     atualizaTempoInicial(data[numeroAleatorio].tempo);
 }
 
-function buscaFrase() {
-    $("#spinner").toggle()
-    var fraseId = $("#frase-id").val();
-    var dados = { id: fraseId };
 
-    $.get("http://localhost:3000/frases",dados,trocaFrase)
+function buscaFrase() {
+
+    $("#spinner").toggle();
+    var fraseId = $("#frase-id").val();
+
+    //criacao do objeto JS que guarda a id
+    var dados = {id : fraseId}; 
+
+    //passando objecto como segundo parametro
+    $.get("http://localhost:3000/frases", dados, trocaFrase)
     .fail(function(){
         $("#erro").toggle();
         setTimeout(function(){
             $("#erro").toggle();
-        },2000);
-    }).always(function () {
-        $("#spinner").toggle()
+    },2000);
+    })
+    .always(function(){
+        $("#spinner").toggle();
     });
 }
 
 function trocaFrase(data) {
+
+    console.log(data);
+
     var frase = $(".frase");
-    frase.text(data.texto);
+    frase.text(data.texto); //cuidado, texto com "o" no final 
     atualizaTamanhoFrase();
     atualizaTempoInicial(data.tempo);
 }
-
-
